c6d67f8639e9c1ed6714b2ecbe8522c152db5d3d
marfs7599 <marifsulaksono@gmail.com>
2023-07-10 23:41:33 +0700

    delete handler employee, changing concept

--- route.go
+++ route.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"gostore/controller"
 	"gostore/middleware"
-	"gostore/modules"
-	"gostore/transaction"
 
 	"github.com/gorilla/mux"
 )
@@ -13,45 +12,34 @@ func route() *mux.Router {
 	r := mux.NewRouter()
 
 	// ===================== Router Login ======================
-	r.HandleFunc("/register", modules.Register).Methods("POST") // register new user
-	r.HandleFunc("/login", modules.Login).Methods("POST")       // login and create JSON Web Token (JWT)
+	r.HandleFunc("/register", controller.Register).Methods("POST") // register new user
+	r.HandleFunc("/login", controller.Login).Methods("POST")       // login and create JSON Web Token (JWT)
 
 	// ===================== Router Item =======================
-	r.HandleFunc("/items", middleware.JWTMiddleware(modules.GetAllItems)).Methods("GET")               // get all sales & soldout item data
-	r.HandleFunc("/item/{id}", middleware.JWTMiddleware(modules.GetItembyId)).Methods("GET")           // get item data by id
-	r.HandleFunc("/item", middleware.JWTMiddleware(modules.InsertItem)).Methods("POST")                // create new item data
-	r.HandleFunc("/item/{id}", middleware.JWTMiddleware(modules.UpdateItem)).Methods("PUT")            // update item data by id
-	r.HandleFunc("/item/sales", middleware.JWTMiddleware(modules.SalesItem)).Methods("GET")            // get all sale item data
-	r.HandleFunc("/item/sale/{id}", middleware.JWTMiddleware(modules.SaleItem)).Methods("PUT")         // update item to sale
-	r.HandleFunc("/item/soldouts", middleware.JWTMiddleware(modules.SoldoutsItem)).Methods("GET")      // get all soldout item data
-	r.HandleFunc("/item/soldout/{id}", middleware.JWTMiddleware(modules.SoldoutItem)).Methods("PUT")   // update item to soldout
-	r.HandleFunc("/item/{id}", middleware.JWTMiddleware(modules.DeleteItem)).Methods("DELETE")         // delete item data by id
-	r.HandleFunc("/item/category/{id}", middleware.JWTMiddleware(modules.CategoryItem)).Methods("GET") // filter items by category
+	r.HandleFunc("/items", middleware.JWTMiddleware(controller.GetAllItems)).Methods("GET")          // get all sales & soldout item data
+	r.HandleFunc("/item/{id}", middleware.JWTMiddleware(controller.GetItembyId)).Methods("GET")      // get item data by id
+	r.HandleFunc("/item", middleware.JWTMiddleware(controller.InsertItem)).Methods("POST")           // create new item data
+	r.HandleFunc("/item/{id}", middleware.JWTMiddleware(controller.UpdateItem)).Methods("PUT")       // update item data by id
+	r.HandleFunc("/item/sales", middleware.JWTMiddleware(controller.SalesItem)).Methods("GET")       // get all sale item data
+	r.HandleFunc("/item/soldouts", middleware.JWTMiddleware(controller.SoldoutsItem)).Methods("GET") // get all soldout item data
+	r.HandleFunc("/item/{id}", middleware.JWTMiddleware(controller.DeleteItem)).Methods("DELETE")    // delete item data by id
+
+	// ==================== Router Category ====================
+	r.HandleFunc("/item/category/{id}", middleware.JWTMiddleware(controller.GetAllCategory)).Methods("GET") // filter items by category
 
 	// ==================== Router Transaction Item ====================
-	r.HandleFunc("/transactionItems", middleware.JWTMiddleware(transaction.GetTransactionItem)).Methods("GET")
-	r.HandleFunc("/transactionItem/{id}", middleware.JWTMiddleware(transaction.GetTransactionItemId)).Methods("GET")
-	r.HandleFunc("/transactionItem", middleware.JWTMiddleware(transaction.CreateTransactionItem)).Methods("POST")
-	r.HandleFunc("/transactionItem/{id}", middleware.JWTMiddleware(transaction.UpdateTransactionItem)).Methods("PUT")
-	r.HandleFunc("/transactionItem/{id}", middleware.JWTMiddleware(transaction.DeleteTransactionItem)).Methods("DELETE")
+	r.HandleFunc("/transactionItems", middleware.JWTMiddleware(controller.GetTransactionItem)).Methods("GET")
+	r.HandleFunc("/transactionItem/{id}", middleware.JWTMiddleware(controller.GetTransactionItemId)).Methods("GET")
+	r.HandleFunc("/transactionItem", middleware.JWTMiddleware(controller.CreateTransactionItem)).Methods("POST")
+	r.HandleFunc("/transactionItem/{id}", middleware.JWTMiddleware(controller.UpdateTransactionItem)).Methods("PUT")
+	r.HandleFunc("/transactionItem/{id}", middleware.JWTMiddleware(controller.DeleteTransactionItem)).Methods("DELETE")
 
 	// ==================== Router Transaction ====================
-	r.HandleFunc("/transactions", middleware.JWTMiddleware(transaction.GetTransaction)).Methods("GET")
-	r.HandleFunc("/transaction/{id}", middleware.JWTMiddleware(transaction.GetTransactionById)).Methods("GET")
-	r.HandleFunc("/transaction", middleware.JWTMiddleware(transaction.CreateTransaction)).Methods("POST")
-	r.HandleFunc("/transaction/{id}", middleware.JWTMiddleware(transaction.UpdateTransaction)).Methods("PUT")
-	r.HandleFunc("/transaction/{id}", middleware.JWTMiddleware(transaction.DeleteTransaction)).Methods("DELETE")
-
-	// ==================== Router Employee ====================
-	r.HandleFunc("/employees", middleware.JWTMiddleware(modules.GetAllEmployees)).Methods("GET")                // get all active & inactive employee data
-	r.HandleFunc("/employee/{id}", middleware.JWTMiddleware(modules.GetEmployeebyId)).Methods("GET")            // get employee data by id
-	r.HandleFunc("/employees", middleware.JWTMiddleware(modules.InsertEmployee)).Methods("POST")                // create new employee data
-	r.HandleFunc("/employees/{id}", middleware.JWTMiddleware(modules.UpdateEmployee)).Methods("PUT")            // update employee data by id
-	r.HandleFunc("/employees/active", middleware.JWTMiddleware(modules.ActivedEmployee)).Methods("GET")         // get all active employee data
-	r.HandleFunc("/employees/active/{id}", middleware.JWTMiddleware(modules.ActiveEmployee)).Methods("PUT")     // update employee to active
-	r.HandleFunc("/employees/inactive", middleware.JWTMiddleware(modules.InactivedEmployee)).Methods("GET")     // get all non-active employee data
-	r.HandleFunc("/employees/inactive/{id}", middleware.JWTMiddleware(modules.InactiveEmployee)).Methods("PUT") // update employee to non-active
-	r.HandleFunc("/employees/{id}", middleware.JWTMiddleware(modules.DeleteEmployee)).Methods("DELETE")         // delete employee data by id
+	r.HandleFunc("/transactions", middleware.JWTMiddleware(controller.GetTransaction)).Methods("GET")
+	r.HandleFunc("/transaction/{id}", middleware.JWTMiddleware(controller.GetTransactionById)).Methods("GET")
+	r.HandleFunc("/transaction", middleware.JWTMiddleware(controller.CreateTransaction)).Methods("POST")
+	r.HandleFunc("/transaction/{id}", middleware.JWTMiddleware(controller.UpdateTransaction)).Methods("PUT")
+	r.HandleFunc("/transaction/{id}", middleware.JWTMiddleware(controller.DeleteTransaction)).Methods("DELETE")
 
 	return r
 }
