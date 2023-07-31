f6a15dc3b35ad7592a8ced9981a38ed61f77baed
marfs7599 <marifsulaksono@gmail.com>
2023-08-01 02:20:25 +0700

    refactor: add some new errors message

--- helper/error.go
+++ helper/error.go
@@ -3,12 +3,13 @@ package helper
 import "errors"
 
 var (
-	ErrAccDeny             = errors.New("Access Denied!")
+	ErrAccDeny             = errors.New("Access Denied")
 	ErrNotFound            = errors.New("Record not found")
 	ErrRecDeleted          = errors.New("Record was deleted")
 	ErrRecRestored         = errors.New("Record was restored")
 	ErrChangeStatusProduct = errors.New("No status changed")
 	ErrStockNotEnough      = errors.New("Stock product isn't enough")
 	ErrUserExist           = errors.New("Username already exist")
-	ErrInvalidUser         = errors.New("Unauthorized, invalid user!")
+	ErrInvalidUser         = errors.New("Unauthorized, invalid user")
+	ErrDuplicateStore      = errors.New("User allowed has one store only")
 )
