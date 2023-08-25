249356273047b6308436f320fb9e735b3c3e2727
Muhammad Arif Sulaksono <114550996+marifsulaksono@users.noreply.github.com>
2023-08-25 13:01:14 +0700

    refactor:delete file pagination.go
    changed to metadata.go

--- helper/pagination.go
+++
@@ -1,8 +0,0 @@
-package helper
-
-type Page struct {
-	Limit     int `json:"limit,omitempty"`
-	Total     int `json:"total,omitempty"`
-	Page      int `json:"current_page,omitempty"`
-	TotalPage int `json:"total_page,omitempty"`
-}
